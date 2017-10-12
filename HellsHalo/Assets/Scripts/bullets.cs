1d453836f83eea1c7793710c106e82ecb1aaf88c
schadjason08 <schadjason08@gmail.com>
2017-10-12 03:33:01 -0500

    NewUpdate

---
+++ HellsHalo/Assets/Scripts/bullets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullets : MonoBehaviour 
+{
+
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody> ().velocity = transform.up * speed;
+    }
+
+
+}
---
+++ HellsHalo/Assets/Scripts/playerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary1
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class playerScript : MonoBehaviour {
+
+    public float speed;
+
+    public Boundary1 boundary;
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3
+            (
+            Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp (rigidbody.position.y, boundary.yMin, boundary.yMax),
+            0.0f
+
+
+
+
+            );
+
+    }
+	
+}
